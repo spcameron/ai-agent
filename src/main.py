012d0f24de69933b20f5cacbd7c83846790f4bc6
spcameron <cameron.seanpatrick@gmail.com>
2025-06-15 19:19:55 -0400

    Refactor main.py
    This commit slightly refactors main.py to have better encapsulation and
    extensibility for adding more argument flags.

--- src/main.py
+++ src/main.py
@@ -1,37 +1,41 @@
+# main.py
+
 import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
-if sys.argv[1]:
-    user_prompt = sys.argv[1]
-else:
-    print("Error: No prompt provided")
-    
-verbose = False
-if "--verbose" in sys.argv:
-    verbose = True
-    
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=messages,
-)
+    if not args:
+        print("Error: No prompt provided")
+        
+    user_prompt = " ".join(args)
+        
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-metadata = response.usage_metadata
-prompt_tokens = metadata.prompt_token_count
-response_tokens = metadata.candidates_token_count
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages,
+    )
 
-if verbose:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
-    print()
-print(response.text)
+    metadata = response.usage_metadata
+    prompt_tokens = metadata.prompt_token_count
+    response_tokens = metadata.candidates_token_count
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    print(f"\n Response: {response.text}")
+
+if __name__ == "__main__":
+    main()
