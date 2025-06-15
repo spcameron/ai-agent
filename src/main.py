9727df430fa341f7c7e4fbf546867f470e739e2d
spcameron <cameron.seanpatrick@gmail.com>
2025-06-15 15:44:40 -0400

    Update main.py to accept user prompts on CLI
    Instead of hard-coding the prompt, this commit refactors main.py to
    accept a prompt from user input on the CLI. Additionally, if "--verbose"
    is passed as a flag on the CLI, the program will output the user prompt,
    the prompt tokens, and the response tokens, in addition to the AI
    reponse.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,37 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+if sys.argv[1]:
+    user_prompt = sys.argv[1]
+else:
+    print("Error: No prompt provided")
+    
+verbose = False
+if "--verbose" in sys.argv:
+    verbose = True
+    
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001", contents=messages,
 )
 
-print(response.text)
-
 metadata = response.usage_metadata
-prompt_token_count = metadata.prompt_token_count
-candidates_token_count = metadata.candidates_token_count
+prompt_tokens = metadata.prompt_token_count
+response_tokens = metadata.candidates_token_count
 
-print(f"Prompt tokens: {prompt_token_count}")
-print(f"Response tokens: {candidates_token_count}")
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print()
+print(response.text)
