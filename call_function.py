c20e6590d3830e5d85a91f279c9b96aa4fabe384
spcameron <cameron.seanpatrick@gmail.com>
2025-06-16 12:00:40 -0400

    Create call_function file
    This file details the available functions that our AI agent is permitted
    to call.

---
+++ call_function.py
@@ -0,0 +1,12 @@
+# call_function.py
+
+from google.genai import types
+
+from functions.get_files_info import schema_get_files_info
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
