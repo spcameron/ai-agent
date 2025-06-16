c95ef128c00286338572f67910c9cc02b7c7922a
spcameron <cameron.seanpatrick@gmail.com>
2025-06-16 12:01:33 -0400

    Refactor main
    This commit reorganizes main.py to extract the task performed by
    client.models.generate_content to its own function, generate_content().
    Additionally, it prints any function calls that the AI agent requests
    from the available tools, outputting function names and args. Lastly, it
    returns the response.text to main() rather than printing.

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+from call_function import available_functions
+
+
 def main():
     load_dotenv()
-
+    
     verbose = "--verbose" in sys.argv
     args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
@@ -18,24 +22,41 @@ def main():
         print("Error: No prompt provided")
         
     user_prompt = " ".join(args)
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}")
         
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    
+    generate_content(client, messages, verbose)
 
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages,
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+            ),
     )
 
     metadata = response.usage_metadata
     prompt_tokens = metadata.prompt_token_count
     response_tokens = metadata.candidates_token_count
-
+    
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
-    print(f"\n Response: {response.text}")
+
+    if not response.function_calls:
+        return response.text
+    
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")     
+    
 
 if __name__ == "__main__":
     main()
