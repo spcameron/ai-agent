1abc4938715deba5a812bf303bf471a35b6ad35b
spcameron <cameron.seanpatrick@gmail.com>
2025-06-16 13:49:16 -0400

    Implement feedback loop for agent iteration
    This commit refactors generate_content() to include a loop with a
    maximum iteration of 20 passes that can call the LLM over and over,
    while also maintaining a list of messages in the "conversation" to keep
    track of progress. Each iteration checks the .candidates property of the
    response, adding the candidate.content to the messages list.
    Additionally, each function call now appends the types.Content reponse
    from the function to messages as well. After each loop, if a function
    was called, it iterates again (unless the maximum iterations was
    reached). Otherwise, the LLM's final response is printed to output --
    meaning the agent is done with the task (or failed).

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from config import MAX_ITERATIONS
 
 
 def main():
@@ -34,32 +35,43 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
+    for _ in range(MAX_ITERATIONS):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
             ),
-    )
+        )
+        
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    metadata = response.usage_metadata
-    prompt_tokens = metadata.prompt_token_count
-    response_tokens = metadata.candidates_token_count
-    
-    if verbose:
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
+        metadata = response.usage_metadata
+        prompt_tokens = metadata.prompt_token_count
+        response_tokens = metadata.candidates_token_count        
+        
+        if verbose:
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
 
-    if not response.function_calls:
-        return response.text
+        if not response.function_calls:
+            print(f"Final response: {response.text}")
+            return
+        
+        for function_call_part in response.function_calls:
+            function_call_result = call_function(function_call_part, verbose)
+            messages.append(function_call_result)
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception(f"Fatal error: The call to {function_call_part.name} returned no response")
+            elif verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+
     
-    for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose)
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception(f"Fatal error: The call to {function_call_part.name} returned no response")
-        elif verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+
     
 
 if __name__ == "__main__":
